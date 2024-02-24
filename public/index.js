75f9240d5f37aa2c5a9b36ca8410d9b030be8a04
Frank <imbruglia100@gmail.com>
2024-02-24 15:01:33 -0500

    progress

--- public/index.js
+++ public/index.js
@@ -1,1 +1,27 @@
-// Your code here 
+
+
+
+// const getCatPics = async () => {
+//     const catsJSON = await fetch("https://api.thecatapi.com/v1/images/search", {
+//         headers: {
+//                 "x-api-key": "live_1dfrhQCPdRiKF5Uwezi43x2v4LJCyJ2Kp8agVnIUTzs5Tp8BcoG7urQpXDgCj2F3",
+//                 "mode": "no-cors"
+//                 }
+//     })
+//     const catsObj = await catsJSON.json()
+//     const imgCon = document.createElement("img")
+//     imgCon.src = catsObj[0].url
+
+//     document.getElementById("imgCon").appendChild(imgCon)
+
+// }
+
+// const changeCat = (e) => {
+//     document.querySelector("img").remove()
+
+//     getCatPics()
+// }
+
+// getCatPics()
+
+// document.getElementById("button").addEventListener("click", changeCat)
